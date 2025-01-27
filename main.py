282e831f7b7ffa4fc173d7f8e524508d7d30d71c
Dawid Szarwark <daq131@gmail.com>
2025-01-27 16:56:12 -0400

    add chart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Porównanie odsetek kredytowych
 import numpy_financial as npf
 import numpy as np
+import matplotlib.pyplot as plt
 
 freq = 12
 rate = 0.0675
@@ -27,3 +28,21 @@ balance = np.zeros(nper) + pv
 # bilans otwarcia (końcowy :)
 balance_close = np.around(balance - np.cumsum(principal_decreasing),2)
 print(balance_close[[0,1,2,-3,-2,-1]])
+# wektor długu
+print(np.cumsum(principal_decreasing)[:10])
+# obliczyć bilans początkowy, część odsetkową, bilans końcowy
+balance_open = balance_close + principal_decreasing
+# częsci odstekowe
+interest_decreasing = np.around(balance_open * rate,2)
+print(interest_decreasing[:10])
+# porównanie sumy zapłaconych odsetek
+print("Wartość odsetek do zapłaty w wariancie kredytu w równych ratach wynosi: " + str("{:.2f}".format(interest_equal.sum())))
+print("Wartość odsetek do zapłaty w wariancie kredytu w ratach malejących wynosi: " + str("{:.2f}".format(interest_decreasing.sum())))
+
+# wykres danych
+plt.plot(interest_equal.cumsum(),label='raty równe')
+plt.plot(interest_decreasing.cumsum(),label='raty malejące')
+plt.legend()
+plt.xlabel('Liczba okresów')
+plt.ylabel('Skumulowana wartość odsetek')
+plt.show()
